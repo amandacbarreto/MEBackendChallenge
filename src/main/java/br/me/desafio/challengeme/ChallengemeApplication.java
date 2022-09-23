9d659012bbe980efaa7b83ade2fc2edc4e694759
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-09-23 09:33:28 -0300

    Created project

---
+++ src/main/java/br/me/desafio/challengeme/ChallengemeApplication.java
@@ -0,0 +1,13 @@
+package br.me.desafio.challengeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/me/desafio/challengeme/ChallengemeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.me.desafio.challengeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
