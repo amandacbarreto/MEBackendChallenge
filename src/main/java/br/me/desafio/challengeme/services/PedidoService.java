f476a2e7b67f718588c3dd5f40f980bb62f02a7e
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-09-28 09:21:22 -0300

    Criação da camada de serviços de Pedido (métodos findAll e findById)

---
+++ src/main/java/br/me/desafio/challengeme/services/PedidoService.java
@@ -0,0 +1,25 @@
+package br.me.desafio.challengeme.services;
+
+import br.me.desafio.challengeme.entities.Pedido;
+import br.me.desafio.challengeme.repositories.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository repository;
+
+    public List<Pedido> findAll() {
+        return repository.findAll();
+    }
+
+    public Pedido findById(Long id){
+        Optional<Pedido> obj = repository.findById(id);
+        return obj.get();
+    }
+}
