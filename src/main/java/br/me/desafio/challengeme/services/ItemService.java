e2efdedbca748bc88da5c5cf6b4cb3cb57387f2c
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-09-28 14:55:19 -0300

    Tratamento de exceção - delete da Entidade Item

--- src/main/java/br/me/desafio/challengeme/services/ItemService.java
+++ src/main/java/br/me/desafio/challengeme/services/ItemService.java
@@ -2,8 +2,11 @@ package br.me.desafio.challengeme.services;
 
 import br.me.desafio.challengeme.entities.Item;
 import br.me.desafio.challengeme.repositories.ItemRepository;
+import br.me.desafio.challengeme.services.exceptions.DatabaseException;
 import br.me.desafio.challengeme.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,11 +32,18 @@ public class ItemService {
     }
 
     public void delete (Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public Item update (Long id, Item obj){
-        Item entity = repository.getOne(id);
+        Item entity = repository.getReferenceById(id);
         updateData(entity, obj);
         return repository.save(entity);
     }
