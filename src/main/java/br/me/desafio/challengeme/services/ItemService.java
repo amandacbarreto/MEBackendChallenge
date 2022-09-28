7cd0f0546e4ff6bb4a07ef128b256e9722ea93c3
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-09-28 14:58:45 -0300

    Tratamento de exceção - update da Entidade Item

--- src/main/java/br/me/desafio/challengeme/services/ItemService.java
+++ src/main/java/br/me/desafio/challengeme/services/ItemService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,9 +44,13 @@ public class ItemService {
     }
 
     public Item update (Long id, Item obj){
-        Item entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try{
+            Item entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(Item entity, Item obj) {
