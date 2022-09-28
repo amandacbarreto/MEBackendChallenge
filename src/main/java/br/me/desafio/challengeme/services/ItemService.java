5be461572329b5a952409be2e4c442543a8f1cd1
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-09-28 12:55:09 -0300

    Método DELETE de Item

--- src/main/java/br/me/desafio/challengeme/services/ItemService.java
+++ src/main/java/br/me/desafio/challengeme/services/ItemService.java
@@ -26,4 +26,8 @@ public class ItemService {
     public Item insert (Item obj) {
         return repository.save(obj);
     }
+
+    public void delete (Long id) {
+        repository.deleteById(id);
+    }
 }
